6d64e036fa0e95813be6c18695df08b32968d1e6
Igor X <iverbitskiy321@gmail.com>
2024-02-27 02:40:32 +0300

    create custom editor window + service

--- EndlessWinter/Assets/Code/GameModule/ServiceModule/SaveLoadService/NovelLoadService.cs
+++ EndlessWinter/Assets/Code/GameModule/ServiceModule/SaveLoadService/NovelLoadService.cs
@@ -1,5 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks.Linq;
 using GameModule.SettingsModule;
 using UnityEngine;
 using Dre0Dru.AddressableAssets;
@@ -8,6 +11,11 @@ using Dre0Dru.AddressableAssets.Loaders;
 using GameModule.DataModule;
 using GameModule.StorageModule;
 using Newtonsoft.Json;
+using UnityEditor;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceLocations;
+using UnityEngine.UI;
 
 namespace GameModule.PlayerModule
 {
@@ -16,6 +24,8 @@ namespace GameModule.PlayerModule
 		private readonly NovelStorage _novelStorage;
 		private readonly IAssetsReferenceLoader<TextAsset> _loader;
 		private readonly ChapterLoadSettings _chapterLoadSettings;
+
+		private TextAsset _textAsset;
 		public NovelLoadService(NovelStorage ___novelStorage, ChapterLoadSettings __chapterLoadSettings)
 		{
 			_novelStorage = ___novelStorage;
@@ -27,16 +37,15 @@ namespace GameModule.PlayerModule
 
 		public Task PlaceInStorage()
 		{
-			TextAsset text = _loader.GetAsset(_chapterLoadSettings.TestAssetActor);
-			
-			_novelStorage.SetNewActor(text);
+			//TextAsset text = _loader.GetAsset(_chapterLoadSettings.TestAssetActor);
+			_novelStorage.SetNewActor(_textAsset);
 
 			return Task.CompletedTask;
 		}
 
 		public async UniTask LoadPack()
 		{
-			UniTask<TextAsset[]> r = _loader.LoadAssetsAsync(_chapterLoadSettings.TestAssetBig);
+			/*UniTask<TextAsset> r = _loader.TryGetAsset(_chapterLoadSettings.TestAssetActor);
 			UniTask<TextAsset[]> z = _loader.LoadAssetsAsync(_chapterLoadSettings.TestAssetActor);
 			
 			int x = 0;
@@ -51,19 +60,51 @@ namespace GameModule.PlayerModule
 			
 			Debug.Log("downloaded");
 			var progress = Progress.Create<float>(x => Debug.Log(x));
+			*/
+			AsyncOperationHandle<IList<TextAsset>> l = Addressables.LoadAssetsAsync<TextAsset>("chapter1", Callback);
 			
+			Addressables.Release(l);
+			int x = 0;
+			while (l.Status != AsyncOperationStatus.Succeeded)
+			{
+				await UniTask.Yield();
+				Debug.Log(x);
+				x++;
+			}
 			
-			//AssetLabelsDownloadPack downloadPack = new AssetLabelsDownloadPack(__chapterSettings.AssetLabelTest);
+			await l.Task;
+			//Addressables.LoadResourceLocationsAsync("chapter1", typeof(TextAsset)).Completed += OnCompleted;
+			/*AssetLabelsDownloadPack downloadPack = new AssetLabelsDownloadPack(_chapterLoadSettings.AssetLabelTest);
+			await downloadPack.StartDownloadAsync();
 			
-			//downloadPack.TrackProgress(Progress.Create<AssetsDownloadStatus>(DisplayStatus));
-			
-			//await downloadPack.StartDownloadAsync();
-			
-			
-			Debug.Log(_loader.IsAssetLoaded(_chapterLoadSettings.TestAssetActor));
+			downloadPack.TrackProgress(Progress.Create<AssetsDownloadStatus>(DisplayStatus));*/
+
+
 		}
-		
-		
+		private void Callback(TextAsset __obj)
+		{
+			if (__obj.name == "large-file")
+					return;
+			Debug.Log(__obj.name);
+			Debug.Log(__obj.text);
+			_textAsset = __obj;
+		}
+		private async void OnCompleted(AsyncOperationHandle<IList<IResourceLocation>> __obj)
+		{
+			foreach (IResourceLocation resource in __obj.Result)
+			{
+				var guid = AssetDatabase.AssetPathToGUID(resource.PrimaryKey);
+				
+				AssetReferenceT<TextAsset> s = new AssetReferenceT<TextAsset>(guid);
+				
+				await _loader.LoadAssetAsync(s);
+				
+				Debug.Log(_loader.IsAssetLoaded(_chapterLoadSettings.TestAssetActor));
+
+			}
+		}
+
+
 		private void DisplayStatus(AssetsDownloadStatus status)  
 		{
 			Debug.Log($"Percent: {status.PercentProgress:F}");
--- EndlessWinter/Assets/Editor/NovelEditorWindow.cs
+++ EndlessWinter/Assets/Editor/NovelEditorWindow.cs
@@ -1,72 +1,274 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using GameModule.DataModule;
+using ModestTree;
+using Newtonsoft.Json;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Editor
 {
-    public class NovelEditorWindow : EditorWindow
-    {
-        private VisualElement _root;
-        private StyleSheet _styleSheet;
+	public class NovelEditorWindow : EditorWindow
+	{
+		private List<string>[] textLists = new List<string>[4];
+		private string[] newTexts = new string[4];
+	//	private List<string>[] savedTextList = new List<string>[4];
+		
+		private Vector2 scrollPosition = Vector2.zero;
 
-        private void OnEnable()
-        {
-            _root = rootVisualElement;
-            _styleSheet = (StyleSheet) EditorGUIUtility.Load("NovelEditorWindow.uss");
-            _root.styleSheets.Add(_styleSheet);
-        }
-        
-        [MenuItem("Tools/Novel Editor Window")]
-        public static void ShowWindow()
-        {
-            var window = GetWindow<NovelEditorWindow>();
-            window.titleContent = new GUIContent("Novel Editor");
-            window.minSize = new Vector2(1200, 700);
-        }
+		private VisualElement _root;
+		private StyleSheet _styleSheet;
 
-        public void CreateGUI()
-        {
-            CreateListChapters();
-        }
+		private ListView _chaptersListView;
 
-        private void CreateListChapters()
-        {
-            Box chaptersLabel = new Box();
-            chaptersLabel.AddToClassList("chapters-box");
-            
-            const int itemCount = 30;
-            List<string> items = new List<string>();
-            
-            for (int i = 0; i <= itemCount; i++)
-                items.Add(i.ToString());
-            
-            
-            Func<VisualElement> makeItem = () => new Label();
-            Action<VisualElement, int> bindItem = (e, i) =>
-            {
-                ((Label)e).text = "Chapter " + items[i];
-                ((Label)e).AddToClassList("chapters-items");
-            };
-            
-            ListView listView = new ListView
-            {
-                makeItem = makeItem,
-                bindItem = bindItem,
-                itemsSource = items,
-                selectionType = SelectionType.Single
-            };
-            listView.itemsChosen += Debug.Log;
-            listView.selectionChanged += Debug.Log;
-            
-            
-            chaptersLabel.Add(listView);
-            listView.AddToClassList("chapters-listview");
-            listView.hierarchy.Children().FirstOrDefault()?.AddToClassList("chapters-scrollview");
-            _root.Add(chaptersLabel);
-            //_root.Add(listView);
-        }
-    }
+
+		private VisualElement _chapterParametersBox;
+		private TreeView _charactersTreeView;
+
+		private int _currentChapter;
+		private int _currentCharacter;
+
+		private Actor _rootActor;
+		private void OnEnable()
+		{
+			_root = rootVisualElement;
+			_styleSheet = (StyleSheet)EditorGUIUtility.Load("NovelEditorWindow.uss");
+			_root.styleSheets.Add(_styleSheet);
+		}
+
+		[MenuItem("Tools/Novel Editor Window")]
+		public static void ShowWindow()
+		{
+			var window = GetWindow<NovelEditorWindow>();
+			window.titleContent = new GUIContent("Novel Editor");
+			window.minSize = new Vector2(1200, 700);
+		}
+
+		private void OnGUI()
+		{
+			CreateTextLabels();
+		}
+		
+		public void CreateGUI()
+		{
+			CreateListChapters();
+			CreateCharacters();
+		}
+
+		private void CreateListChapters()
+		{
+			Box chaptersLabel = new Box();
+			chaptersLabel.AddToClassList("chapters-box");
+
+			const int itemCount = 30;
+			List<string> items = new List<string>();
+
+			for (int i = 1; i <= itemCount; i++)
+				items.Add(i.ToString());
+
+
+			Func<VisualElement> makeItem = () => new Label();
+			Action<VisualElement, int> bindItem = (e, i) =>
+			{
+				((Label)e).text = "Chapter " + items[i];
+				((Label)e).AddToClassList("chapters-items");
+			};
+
+			_chaptersListView = new ListView
+			{
+				makeItem = makeItem,
+				bindItem = bindItem,
+				itemsSource = items,
+				selectionType = SelectionType.Single
+			};
+
+			_chaptersListView.itemsChosen += OnChaptersClick;
+			_chaptersListView.selectionChanged += OnChaptersClick;
+
+			chaptersLabel.Add(_chaptersListView);
+			_chaptersListView.AddToClassList("chapters-listview");
+			_chaptersListView.hierarchy.Children().FirstOrDefault()?.AddToClassList("chapters-scrollview");
+			_root.Add(chaptersLabel);
+		}
+
+		private void CreateCharacters()
+		{
+			_chapterParametersBox = new Box();
+			_chapterParametersBox.AddToClassList("chapter-parameters-box");
+
+			var charactersItems = new List<TreeViewItemData<string>>(1);
+
+			var treeViewSubCharactersData = new List<TreeViewItemData<string>>(5);
+			treeViewSubCharactersData.Add(new TreeViewItemData<string>(0, "Player"));
+			treeViewSubCharactersData.Add(new TreeViewItemData<string>(1, "Avtor"));
+			treeViewSubCharactersData.Add(new TreeViewItemData<string>(2, "Quest"));
+			treeViewSubCharactersData.Add(new TreeViewItemData<string>(3, "Anna"));
+			treeViewSubCharactersData.Add(new TreeViewItemData<string>(4, "Elena"));
+
+			var treeViewItemData = new TreeViewItemData<string>(110, "Characters", treeViewSubCharactersData);
+			charactersItems.Add(treeViewItemData);
+
+			Func<VisualElement> makeItem = () => new Label();
+			Action<VisualElement, int> bindItem = (e, i) =>
+			{
+				var item = (_charactersTreeView).GetItemDataForIndex<string>(i);
+				(e as Label).text = item;
+			};
+
+			_charactersTreeView = new TreeView();
+
+			_charactersTreeView.SetRootItems(charactersItems);
+			_charactersTreeView.makeItem = makeItem;
+			_charactersTreeView.bindItem = bindItem;
+			_charactersTreeView.selectionType = SelectionType.Multiple;
+			_charactersTreeView.Rebuild();
+
+			_charactersTreeView.selectedIndicesChanged += OnCharacterClick;
+
+			_charactersTreeView.AddToClassList("characters-treeview");
+			_chapterParametersBox.Add(_charactersTreeView);
+
+			_charactersTreeView.visible = false;
+			_chapterParametersBox.visible = false;
+			_root.Add(_chapterParametersBox);
+		}
+
+		private void CreateTextLabels()
+		{
+			GUILayout.BeginArea(new Rect(300, 0, position.width - 300, position.height));
+			
+			scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+			GUILayout.BeginHorizontal();
+
+			for (int i = 0; i < 4; i++)
+			{
+				GUILayout.BeginVertical();
+
+				// Поле для ввода текста
+				newTexts[i] = EditorGUILayout.TextField("Text " + (i + 1) + ":", newTexts[i]);
+
+				GUILayout.Space(5);
+
+				// Кнопка для добавления элемента в список
+				if (GUILayout.Button("Add " + (i + 1)))
+				{
+					if (!string.IsNullOrEmpty(newTexts[i]))
+					{
+						if (textLists[i] == null)
+						{
+							textLists[i] = new List<string>();
+						}
+						textLists[i].Add(newTexts[i]);
+						newTexts[i] = "";
+					}
+				}
+
+				// Вывод текста в UI элемент
+				if (textLists[i] != null)
+				{
+					foreach (string text in textLists[i])
+					{
+						EditorGUILayout.LabelField("Text " + (i + 1) + ":", text);
+					}
+				}
+
+				GUILayout.EndVertical();
+			}
+
+			GUILayout.EndHorizontal();
+
+			EditorGUILayout.EndScrollView();
+
+			GUILayout.Space(10);
+
+			
+			// Кнопка для добавления всех введенных полей
+			if (GUILayout.Button("Save All"))
+			{
+				string actorName = "Player";
+
+				Queue<string> startReplicas = new Queue<string>();
+				Queue<string> positiveReplicas = new Queue<string>();
+				Queue<string> negativeReplicas = new Queue<string>();
+				Queue<string> endReplicas = new Queue<string>();
+				
+				for (int i = 0; i < textLists.Length; i++)
+				{
+					if(textLists[i] == null || textLists[i].IsEmpty())
+						continue;
+					for (int j = 0; j < textLists[i].Count; j++)
+					{
+						if(i==0)
+							startReplicas.Enqueue(textLists[i][j]);
+						else if(i==1)
+							positiveReplicas.Enqueue(textLists[i][j]);
+						else if(i==2)
+							negativeReplicas.Enqueue(textLists[i][j]);
+						else if(i==3)
+							endReplicas.Enqueue(textLists[i][j]);
+					}
+				}
+				_rootActor = new Actor(actorName, startReplicas, positiveReplicas, negativeReplicas, endReplicas);
+				Debug.Log(_rootActor.startReplicas.Dequeue());
+			}
+
+			// Кнопка для сериализации и сохранения в файле JSON
+			if (GUILayout.Button("Save Text List as JSON"))
+			{
+				string json = JsonConvert.SerializeObject(_rootActor, Formatting.Indented);
+				SaveJsonToFile(json);
+			}
+			GUILayout.Space(15);
+			
+			// Кнопка для очистки всех полей
+			if (GUILayout.Button("Clear All Text Fields"))
+			{
+				for (int i = 0; i < 4; i++)
+				{
+					newTexts[i] = "";
+					if (textLists[i] != null)
+					{
+						textLists[i].Clear();
+					}
+				}
+			}
+			
+			GUILayout.EndArea();
+		}
+		private void SaveJsonToFile(string json)
+		{
+			string path = EditorUtility.SaveFilePanel("Save Text List as JSON", "", "textList", "json");
+			if (path.Length != 0)
+			{
+				File.WriteAllText(path, json);
+				Debug.Log("Text List saved as JSON: " + path);
+			}
+		}
+		
+		private void OnChaptersClick(object __sender)
+		{
+			Debug.Log("Chapter " + _chaptersListView.selectedIndex);
+
+			_currentCharacter = _chaptersListView.selectedIndex;
+
+			_charactersTreeView.visible = true;
+			_chapterParametersBox.visible = true;
+		}
+
+		private void OnCharacterClick(object __sender)
+		{
+			if (_charactersTreeView.selectedIndex == 0)
+				return;
+
+			var insideIndex = _charactersTreeView.selectedIndex - 1;
+
+			Debug.Log("Character " + insideIndex);
+
+			_currentCharacter = insideIndex;
+		}
+		
+	}
 }
